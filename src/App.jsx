568e676859da96ac068e92779c86d239c0cf085b
Tobias Diez <110622068+todiez@users.noreply.github.com>
2023-05-24 11:05:39 +0200

    script updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello There!</h1>,
+    loader: rootLoader,   
+  },
+]);
+
+
 function App() {
-  return <div className="App">asdfj</div>;
+  return <div className="App">
+      <RouterProvider router={router} />
+
+  </div>;
 }
 
 export default App;
