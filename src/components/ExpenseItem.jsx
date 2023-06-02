b1584ab819baaab8886ecba449c28c3be495fa76
todiez <tobias@diez.guru>
2023-06-02 15:41:35 +0200

    showing buget on dashboard only not on budgetpage

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -9,9 +9,7 @@ import {
 } from "../helpers";
 import { TrashIcon } from "@heroicons/react/24/solid";
 
-
-
-const ExpenseItem = ({ expense }) => {
+const ExpenseItem = ({ expense, showBudget }) => {
   const fetcher = useFetcher();
 
   const budget = getAllMatchingItems({
@@ -25,14 +23,20 @@ const ExpenseItem = ({ expense }) => {
       <td>{expense.name}</td>
       <td>{formatCurrency(expense.amount)}</td>
       <td>{formateDateToLocaleString(expense.createdAt)}</td>
-      <td>
-        <Link to={`/budget/${budget.id}`} style={{ "--accent": budget.color }}>
-          {budget.name}
-        </Link>
-      </td>
+      {showBudget && (
+        <td>
+          <Link
+            to={`/budget/${budget.id}`}
+            style={{ "--accent": budget.color }}
+          >
+            {budget.name}
+          </Link>
+        </td>
+      )}
+
       <td>
         {/* fetcher.Form instead of form can handle multiple actions at the same time, necessary e.g. for delay of database or api or sth */}
-        <fetcher.Form method="post"> 
+        <fetcher.Form method="post">
           {/* what to do with the form */}
           <input type="hidden" name="_action" value="deleteExpense" />
           {/* deleting the actual data */}
--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -4,28 +4,55 @@ import { useLoaderData } from "react-router-dom";
 //helpers imports
 import { getAllMatchingItems } from "../helpers";
 
+//components import
+import BudgetItem from "../components/BudgetItem";
+import AddExpenseForm from "../components/AddExpenseForm";
+import Table from "../components/Table";
+
 //loader
-export async function budgetLoader({params}) {
-    const budget = await getAllMatchingItems({
-        category: "budgets",
-        key: "id",
-        value: params.id,
-    })[0];
-    //getAllMatchingItems gives an Array back.. we want just the first item, therefore [0] added
+export async function budgetLoader({ params }) {
+  const budget = await getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: params.id,
+  })[0];
+  //getAllMatchingItems gives an Array back.. we want just the first item, therefore [0] added
 
-    if(!budget) {
-        throw new Error("The buget your are trying to find doesn't exist")
-    }
+  const expenses = await getAllMatchingItems({
+    category: "expenses",
+    key: "budgetId",
+    value: params.id,
+  });
 
-    return {budget};
+  if (!budget) {
+    throw new Error("The buget your are trying to find doesn't exist");
+  }
+
+  return { budget, expenses };
 }
 
 const BudgetPage = () => {
-    const {budget} = useLoaderData();
+  const { budget, expenses } = useLoaderData();
 
   return (
-    <div>Budget: {budget.name}</div>
-  )
-}
+    <div className="grid-lg" style={{ "--accent": budget.color }}>
+      <h1 className="h2">
+        <span className="accent">{budget.name}</span> Overview
+      </h1>
+      <div className="flex-lg">
+        <BudgetItem budget={budget} />
+        <AddExpenseForm budgets={[budget]} />
+      </div>
+      {expenses && expenses.length > 0 && (
+        <div className="grid-md">
+          <h2>
+            <span className="accent">{budget.name}</span> Expenses
+          </h2>
+          <Table expenses={expenses} showBudget={false} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default BudgetPage;
