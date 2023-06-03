7ef0655fcbc68498e7168da720a44af1071a6f09
todiez <tobias@diez.guru>
2023-06-03 12:47:49 +0200

    delete budget finalized

--- src/actions/deleteBudget.js
+++ src/actions/deleteBudget.js
@@ -2,7 +2,10 @@
 import { toast } from "react-toastify";
 
 //helpers
-import { deleteItem } from "../helpers";
+import { deleteItem, getAllMatchingItems } from "../helpers";
+
+//rrd import
+import { redirect } from "react-router-dom";
 
 export function deleteBudget({ params }) {
   //alert("inside deleteBuget")
@@ -13,8 +16,24 @@ export function deleteBudget({ params }) {
       id: params.id,
     });
 
+    const associatedExpenses = getAllMatchingItems({
+      category: "expenses",
+      key: "budgetId",
+      value: params.id,
+    });
+
+    associatedExpenses.forEach((expense) => {
+      deleteItem({
+        key: "expenses",
+        id: expense.id,
+      });
+    });
+
     toast.success("Budget deleted successfully!");
   } catch (e) {
     throw new Error("There was a problem deleting your budget. Sorry.");
   }
+
+  return redirect("/");
+
 }
